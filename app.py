7e58bc82b625c85694630c0c64fc8e6f208508ad
suimi <1044471195@qq.com>
2026-02-04 12:38:01 +0800

    Configure_port

--- app.py
+++ app.py
@@ -103,4 +103,5 @@ if __name__ == '__main__':
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
         
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
