6e626f230a0097667c244f04a20669f49ea8cc58
suimi <1044471195@qq.com>
2026-02-04 12:40:04 +0800

    Update_to_8080

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ if __name__ == '__main__':
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
         
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
